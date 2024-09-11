b9b15390bb8555be1e82f282fdc453e49838e6d3
Ramy Ardya <161923530+UltraRamy@users.noreply.github.com>
2024-09-11 10:21:58 +0700

    Update settings.py

--- eshop_pbp/settings.py
+++ eshop_pbp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-h-oxpjgx)h*r-1r1^1^$mv9tqd_731p#=bqa*l9sddvy6o!xy^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ramy-ardya-eshoppbp.pbp.cs.ui.ac.id"]
 
 
 # Application definition
